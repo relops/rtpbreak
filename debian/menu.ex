586f22e477ec19be43f1d1976c46730467a75a9b
Devon Kearns <dookie@kali.org>
2012-12-30 18:09:20 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rtpbreak):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rtpbreak" command="/usr/bin/rtpbreak"
--- debian/rtpbreak.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rtpbreak package
-#
-0 4	* * *	root	[ -x /usr/bin/rtpbreak_maintenance ] && /usr/bin/rtpbreak_maintenance
